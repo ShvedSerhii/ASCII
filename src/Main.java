ccbb7cd11899967206600f62c03e50b501e328aa
Serhii Shved <shved.serhii@gmail.com>
2020-07-10 03:44:10 +0300

    ascii

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] inputValue = input();
+	    int[] evenValues = Arrays.stream(inputValue)
+                .distinct()
+                .filter(n -> n % 2 == 0)
+                .filter(n -> n <= 127)
+                .toArray();
+        int[] oddValues = Arrays.stream(inputValue)
+                .distinct()
+                .filter(n -> n % 2 != 0)
+                .filter(n -> n <= 127)
+                .toArray();
+	    showResult(evenValues, oddValues);
+    }
+
+    private static int[] input() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input your value: ");
+        char[] charArray = in.next().toUpperCase().toCharArray();
+        int[] result = new int[charArray.length];
+        Arrays.setAll(result, i -> (int)charArray[i]);
+        return result;
+    }
+
+    private static void showResult(int[] evenValues, int[] oddValues) {
+        System.out.println("Even values: ");
+        showValues(evenValues);
+        System.out.println("* * * * * * *");
+        System.out.println("Odd values: ");
+        showValues(oddValues);
+        System.out.println("* * * * * * *");
+        System.out.println("Difference: " + findDifference(evenValues, oddValues));
+    }
+
+    private static void showValues(int[] values) {
+        for (int v : values) {
+            System.out.println((char)v + " -> ASCII " + v);
+        }
+    }
+
+    private static int findDifference(int[] evenValues, int[] oddValues) {
+        int sumEvenValues = Arrays.stream(evenValues).sum();
+        int sumOddValues = Arrays.stream(oddValues).sum();
+        return Math.abs(sumEvenValues - sumOddValues);
+    }
+}
